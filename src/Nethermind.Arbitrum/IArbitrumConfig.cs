fab562c2db79e6637d236b5a5f17a4fb3d0c7ca4
hudem1 <55464342+hudem1@users.noreply.github.com>
2025-05-20 16:03:25 +0900

    feat: Parse L1MessageType_Initialize for chain specs (#18)
    * Migrate basic logic of parsing Arbitrum transactions passed by Nitro
    
    * Address remarks
    
    * Address remarks 2
    
    * Fix max depth check to be in a right place
    
    * Address remarks 3
    
    * feat: Parse L1MessageType_Initialize
    
    * fix: Throw error if necessary
    
    * fix: switch case 0 instead of 2
    
    * fix: Remove DTO suffix
    
    * fix: Use System.Text.Json instead of Newtonsoft
    
    * fix: Add description for test blob
    
    ---------
    
    Co-authored-by: Pavlo Rytikov <pavlo@nethermind.io>

--- src/Nethermind.Arbitrum/IArbitrumConfig.cs
+++ src/Nethermind.Arbitrum/IArbitrumConfig.cs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using Nethermind.Config;
+using Nethermind.Core;
+using Newtonsoft.Json;
 
 namespace Nethermind.Arbitrum
 {
@@ -10,7 +12,25 @@ namespace Nethermind.Arbitrum
         [ConfigItem(Description = "Whether to enable the Arbitrum endpoints.", DefaultValue = "false")]
         bool Enabled { get; set; }
 
+        [ConfigItem(Description = "Whether to allow debug precompiles.", DefaultValue = "false")]
+        bool AllowDebugPrecompiles { get; set; }
+
+        [ConfigItem(Description = "Whether to allow data availability committee.", DefaultValue = "false")]
+        bool DataAvailabilityCommittee { get; set; }
+
+        [ConfigItem(Description = "Initial ArbOS version.", DefaultValue = "0")]
+        ulong InitialArbOSVersion { get; set; }
+
+        [ConfigItem(Description = "Initial chain owner.", DefaultValue = "0x0"), JsonProperty(nameof(InitialChainOwner))]
+        Address InitialChainOwner { get; set; }
+
         [ConfigItem(Description = "Genesis block number.", DefaultValue = "0")]
         public ulong GenesisBlockNum { get; set; }
+
+        [ConfigItem(Description = "Maximum bytecode to permit for a contract. 0 value implies params.DefaultMaxCodeSize.", DefaultValue = "0")]
+        public ulong? MaxCodeSize { get; set; }
+
+        [ConfigItem(Description = "Maximum initcode to permit in a creation transaction and create instructions. 0 value implies params.DefaultMaxInitCodeSize.", DefaultValue = "0")]
+        public ulong? MaxInitCodeSize { get; set; }
     }
 }
