39bb48fcf53ae93c58d12f67147243f0437e1ffd
Pavlo Rytikov <pavlo@nethermind.io>
2025-05-28 12:50:26 +0300

    Remove redundant mocks

--- src/Nethermind.Arbitrum.Tests/Arbos/ArbosGenesisLoaderTests.cs
+++ src/Nethermind.Arbitrum.Tests/Arbos/ArbosGenesisLoaderTests.cs
@@ -1,5 +1,4 @@
 using FluentAssertions;
-using Nethermind.Api;
 using Nethermind.Arbitrum.Data;
 using Nethermind.Arbitrum.Genesis;
 using Nethermind.Arbitrum.Tests.Infrastructure;
@@ -10,7 +9,6 @@ using Nethermind.Logging;
 using Nethermind.Specs.ChainSpecStyle;
 using Nethermind.State;
 using Nethermind.Trie.Pruning;
-using NSubstitute;
 
 namespace Nethermind.Arbitrum.Tests.Arbos;
 
@@ -22,16 +20,7 @@ public class ArbosGenesisLoaderTests
     public void ArbitrumGenesisLoader_FullChainSimulation_ProducesCorrectHash()
     {
         ChainSpec chainSpec = FullChainSimulationChainSpecProvider.Create();
-
-        INethermindApi? api = Substitute.For<INethermindApi>();
-        api.LogManager.Returns(Logger);
-        api.ChainSpec.Returns(chainSpec);
-        api.SpecProvider.Returns(FullChainSimulationSpecProvider.Instance);
-
-        WorldState provider = new(new TrieStore(new MemDb(), Logger), new MemDb(), Logger);
-        IMainProcessingContext? processingContext = Substitute.For<IMainProcessingContext>();
-        processingContext.WorldState.Returns(provider);
-        api.MainProcessingContext.Returns(processingContext);
+        WorldState worldState = new(new TrieStore(new MemDb(), Logger), new MemDb(), Logger);
 
         ArbitrumConfig arbitrumConfig = new()
         {
@@ -49,12 +38,12 @@ public class ArbosGenesisLoaderTests
                 "7b22636861696e4964223a3431323334362c22686f6d657374656164426c6f636b223a302c2264616f466f726b537570706f7274223a747275652c22656970313530426c6f636b223a302c2265697031353048617368223a22307830303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030222c22656970313535426c6f636b223a302c22656970313538426c6f636b223a302c2262797a616e7469756d426c6f636b223a302c22636f6e7374616e74696e6f706c65426c6f636b223a302c2270657465727362757267426c6f636b223a302c22697374616e62756c426c6f636b223a302c226d756972476c6163696572426c6f636b223a302c226265726c696e426c6f636b223a302c226c6f6e646f6e426c6f636b223a302c22636c69717565223a7b22706572696f64223a302c2265706f6368223a307d2c22617262697472756d223a7b22456e61626c654172624f53223a747275652c22416c6c6f774465627567507265636f6d70696c6573223a747275652c2244617461417661696c6162696c697479436f6d6d6974746565223a66616c73652c22496e697469616c4172624f5356657273696f6e223a33322c22496e697469616c436861696e4f776e6572223a22307835453134393764443166303843383762326438464532336539414142366331446538333344393237222c2247656e65736973426c6f636b4e756d223a307d7d"));
 
         ArbitrumGenesisLoader genesisLoader = new(
-            api.ChainSpec,
-            api.SpecProvider!,
-            api.MainProcessingContext!.WorldState,
+            chainSpec,
+            FullChainSimulationSpecProvider.Instance,
+            worldState,
             initMessage,
             arbitrumConfig,
-            api.LogManager);
+            LimboLogs.Instance);
 
         Block genesisBlock = genesisLoader.Load();
 
