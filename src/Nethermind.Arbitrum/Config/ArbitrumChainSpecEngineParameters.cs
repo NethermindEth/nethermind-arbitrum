be6576645c48cad7f0ee545848d58fd805b4b9b0
AnkushinDaniil <ankushin.daniil42@gmail.com>
2025-06-04 15:37:46 +0400

    fix: format

--- src/Nethermind.Arbitrum/Config/ArbitrumChainSpecEngineParameters.cs
+++ src/Nethermind.Arbitrum/Config/ArbitrumChainSpecEngineParameters.cs
@@ -40,7 +40,7 @@ namespace Nethermind.Arbitrum.Config;
 public class ArbitrumChainSpecEngineParameters : IChainSpecEngineParameters
 {
     public const string ArbitrumEngineName = "Arbitrum";
-    
+
     public string? EngineName => SealEngineType;
     public string? SealEngineType => ArbitrumEngineName;
 
