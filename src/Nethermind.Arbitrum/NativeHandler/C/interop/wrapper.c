b10dfa41d4ce5c1b8cf0d6a44a08892242293a18
Tanishq Jasoria <jasoriatanishq@gmail.com>
2025-06-09 13:04:46 -0400

    remove commented code

--- src/Nethermind.Arbitrum/NativeHandler/C/interop/wrapper.c
+++ src/Nethermind.Arbitrum/NativeHandler/C/interop/wrapper.c
@@ -14,19 +14,3 @@ UserOutcomeKind stylus_call_export(
 {
     return stylus_call(module, calldata, config, req_handler, evm_data, debug, output, gas, arbos_tag);
 }
-// __attribute__((visibility("default")))
-// UserOutcomeKind stylus_activate_export(
-//     GoSliceData wasm,
-//     uint16_t page_limit,
-//     uint16_t stylus_version,
-//     uint64_t arbos_version,
-//     bool debug,
-//     RustBytes* output,
-//     const Bytes32* codehash,
-//     Bytes32* module_hash_out,
-//     StylusData* stylus_data_out,
-//     uint64_t* gas)
-// {
-//     return stylus_activate(wasm, page_limit, stylus_version, arbos_version, debug,
-//                            output, codehash, module_hash_out, stylus_data_out, gas);
-// }
