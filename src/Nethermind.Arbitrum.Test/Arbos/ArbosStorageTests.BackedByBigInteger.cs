35f111ce028971e9f5a9e883ca3d7524ee20c0c4
Pavlo Rytikov <pavlo@nethermind.io>
2025-06-18 12:57:38 +0300

    Move BigInteger storage tests to a separate file

---
+++ src/Nethermind.Arbitrum.Test/Arbos/ArbosStorageTests.BackedByBigInteger.cs
@@ -0,0 +1,90 @@
+using System.Numerics;
+using FluentAssertions;
+using Nethermind.Arbitrum.Arbos.Storage;
+using Nethermind.Arbitrum.Test.Infrastructure;
+using Nethermind.Core.Crypto;
+
+namespace Nethermind.Arbitrum.Test.Arbos;
+
+public partial class ArbosStorageTests
+{
+    // Test hashes are captured from Nitro's tests
+    [TestCase("0", "0x0000000000000000000000000000000000000000000000000000000000000000")]
+    [TestCase("1", "0x0000000000000000000000000000000000000000000000000000000000000001")]
+    [TestCase("33", "0x0000000000000000000000000000000000000000000000000000000000000021")]
+    [TestCase("31591083", "0x0000000000000000000000000000000000000000000000000000000001e20aab")]
+    [TestCase("-1", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")]
+    [TestCase("-33", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdf")]
+    [TestCase("-31591083", "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffe1df555")]
+    [TestCase(
+        "57896044618658097711785492504343953926634992332820282019728792003956564819967",
+        "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")] // 2^255 - 1
+    [TestCase(
+        "-57896044618658097711785492504343953926634992332820282019728792003956564819968",
+        "0x8000000000000000000000000000000000000000000000000000000000000000")] // -2^255
+    public void SetCheckedStorageBackedByBigInteger_Always_SetsAndGetsTheSameValue(string strValue, string expectedHash)
+    {
+        const ulong offset = 0ul;
+        (ArbosStorage storage, _) = TestArbosStorage.Create(TestAccount);
+        ArbosStorageBackedBigInteger backedStorage = new(storage, offset);
+
+        BigInteger value = BigInteger.Parse(strValue);
+        backedStorage.SetChecked(value);
+
+        BigInteger storedBigInteger = backedStorage.Get();
+        ValueHash256 storedHash = storage.Get(offset);
+
+        storedBigInteger.Should().Be(value);
+        storedHash.Should().Be(new ValueHash256(expectedHash));
+    }
+
+    [TestCase("57896044618658097711785492504343953926634992332820282019728792003956564819968")] // 2^255
+    [TestCase("-57896044618658097711785492504343953926634992332820282019728792003956564819969")] // -2^255 - 1
+    [TestCase("115792089237316195423570985008687907853269984665640564039457584007913129639934")] // 2^256 * 2
+    [TestCase("-115792089237316195423570985008687907853269984665640564039457584007913129639936")] // (-2^255 - 1) * 2
+    [TestCase("max")]
+    [TestCase("min")]
+    public void SetCheckedStorageBackedByBigInteger_Overflow_Throws(string strValue)
+    {
+        const ulong offset = 0ul;
+        (ArbosStorage storage, _) = TestArbosStorage.Create(TestAccount);
+        ArbosStorageBackedBigInteger backedStorage = new(storage, offset);
+
+        BigInteger value = strValue switch
+        {
+            "max" => BigInteger.Pow(2, 1024) - 1,
+            "min" => BigInteger.Pow(2, 1024) * -1,
+            _ => BigInteger.Parse(strValue)
+        };
+
+        Action action = () => backedStorage.SetChecked(value);
+        action.Should().Throw<OverflowException>();
+    }
+
+    // Test hashes are captured from Nitro's tests
+    [TestCase("0", "0x0000000000000000000000000000000000000000000000000000000000000000")]
+    [TestCase("1", "0x0000000000000000000000000000000000000000000000000000000000000001")]
+    [TestCase("33", "0x0000000000000000000000000000000000000000000000000000000000000021")]
+    [TestCase("31591083", "0x0000000000000000000000000000000000000000000000000000000001e20aab")]
+    [TestCase("-1", "0x0000000000000000000000000000000000000000000000000000000000000001")]
+    [TestCase("-33", "0x0000000000000000000000000000000000000000000000000000000000000021")]
+    [TestCase("-31591083", "0x0000000000000000000000000000000000000000000000000000000001e20aab")]
+    [TestCase(
+        "57896044618658097711785492504343953926634992332820282019728792003956564819967",
+        "0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")]
+    [TestCase(
+        "-57896044618658097711785492504343953926634992332820282019728792003956564819968",
+        "0x8000000000000000000000000000000000000000000000000000000000000000")]
+    public void SetPreVersion7StorageBackedByBigInteger_Always_HandlesNegativeValuesIncorrectly(string strValue, string expectedHash)
+    {
+        const ulong offset = 0ul;
+        (ArbosStorage storage, _) = TestArbosStorage.Create(TestAccount);
+        ArbosStorageBackedBigInteger backedStorage = new(storage, offset);
+
+        BigInteger value = BigInteger.Parse(strValue);
+        backedStorage.SetPreVersion7(value);
+
+        ValueHash256 storedHash = storage.Get(offset);
+        storedHash.Should().Be(new ValueHash256(expectedHash));
+    }
+}
