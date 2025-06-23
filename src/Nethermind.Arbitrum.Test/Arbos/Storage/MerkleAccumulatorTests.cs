e9f7a315fb31e495e92aa9c1b81a916c4c94c240
Pavlo Rytikov <pavlo@nethermind.io>
2025-06-23 16:12:06 +0300

    Implement merkle accumulator (#53)
    * Move AddressSetTests and StorageQueueTests to Storage directory
    
    * Add ComputeKeccak to ArbosStorage, add missing tests
    
    * Remove unused Initialize, implement accumulator, add tests
    
    * Extract constant for the first partial index
    
    * Fix merkle root calculation
    
    * Use array instead of list
    
    * Optimize root calculation to avoid allocations from Bytes.Concat

---
+++ src/Nethermind.Arbitrum.Test/Arbos/Storage/MerkleAccumulatorTests.cs
@@ -0,0 +1,144 @@
+using System.Security.Cryptography;
+using FluentAssertions;
+using Nethermind.Arbitrum.Arbos.Storage;
+using Nethermind.Arbitrum.Test.Infrastructure;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Extensions;
+
+namespace Nethermind.Arbitrum.Test.Arbos.Storage;
+
+public class MerkleAccumulatorTests
+{
+    [Test]
+    public void CalculateRoot_EmptyAccumulator_ReturnsZeroHash()
+    {
+        (ArbosStorage storage, _) = TestArbosStorage.Create();
+        MerkleAccumulator accumulator = new(storage);
+
+        accumulator.CalculateRoot().Should().Be(default);
+    }
+
+    [Test]
+    public void CalculateRoot_SingleNode_ReturnsCorrectRootAndSize()
+    {
+        (ArbosStorage storage, _) = TestArbosStorage.Create();
+        MerkleAccumulator accumulator = new(storage);
+
+        ValueHash256 node = new(RandomNumberGenerator.GetBytes(32));
+        ValueHash256 expected = Keccak.Compute(node.Bytes);
+
+        accumulator.Append(node).Should().BeEmpty();
+
+        accumulator.GetSize().Should().Be(1);
+        accumulator.CalculateRoot().Should().Be(expected);
+    }
+
+    [Test]
+    public void CalculateRoot_3Nodes_ReturnsCorrectRootAndSize()
+    {
+        (ArbosStorage storage, _) = TestArbosStorage.Create();
+        MerkleAccumulator accumulator = new(storage);
+
+        ValueHash256 node1 = new(RandomNumberGenerator.GetBytes(32));
+        ValueHash256 node2 = new(RandomNumberGenerator.GetBytes(32));
+        ValueHash256 node3 = new(RandomNumberGenerator.GetBytes(32));
+
+        ValueHash256 expectedLevel1 = Keccak.Compute(Bytes.Concat(
+            Keccak.Compute(node1.Bytes).Bytes,
+            Keccak.Compute(node2.Bytes).Bytes));
+        ValueHash256 expectedRoot = Keccak.Compute(Bytes.Concat(
+            expectedLevel1.Bytes,
+            Keccak.Compute(Bytes.Concat(
+                Keccak.Compute(node3.Bytes).Bytes,
+                new byte[32])).Bytes));
+
+        accumulator.Append(node1).Should().BeEmpty(); // Add level 0 node
+        accumulator.Append(node2).Should().BeEquivalentTo([
+            new MerkleTreeNodeEvent(1ul, 1ul, expectedLevel1) // Level 1 calculated from 2 nodes at level 0
+        ]);
+        accumulator.Append(node3).Should().BeEmpty(); // Add level 0 node, no new event as no new level is created
+
+        accumulator.GetSize().Should().Be(3);
+        accumulator.CalculateRoot().Should().Be(expectedRoot);
+    }
+
+    [Test]
+    public void CalculateRoot_4Nodes_ReturnsCorrectRootAndSize()
+    {
+        (ArbosStorage storage, _) = TestArbosStorage.Create();
+        MerkleAccumulator accumulator = new(storage);
+
+        ValueHash256 node1 = new(RandomNumberGenerator.GetBytes(32));
+        ValueHash256 node2 = new(RandomNumberGenerator.GetBytes(32));
+        ValueHash256 node3 = new(RandomNumberGenerator.GetBytes(32));
+        ValueHash256 node4 = new(RandomNumberGenerator.GetBytes(32));
+
+        ValueHash256 expectedLevel1First = Keccak.Compute(Bytes.Concat(
+            Keccak.Compute(node1.Bytes).Bytes,
+            Keccak.Compute(node2.Bytes).Bytes));
+        ValueHash256 expectedLevel1Second = Keccak.Compute(Bytes.Concat(
+            Keccak.Compute(node3.Bytes).Bytes,
+            Keccak.Compute(node4.Bytes).Bytes));
+        ValueHash256 expectedTreeRoot = Keccak.Compute(
+            Bytes.Concat(
+                expectedLevel1First.Bytes,
+                expectedLevel1Second.Bytes));
+
+        accumulator.Append(node1).Should().BeEmpty(); // Add level 0 node
+        accumulator.Append(node2).Should().BeEquivalentTo([
+            new MerkleTreeNodeEvent(1ul, 1ul, expectedLevel1First) // Level 1 calculated from 2 nodes at level 0
+        ]);
+        accumulator.Append(node3).Should().BeEmpty(); // Add level 0 node
+        accumulator.Append(node4).Should().BeEquivalentTo([
+            new MerkleTreeNodeEvent(1ul, 3ul, expectedLevel1Second), // Level 1 calculated from 2 nodes at level 0 (second pair)
+            new MerkleTreeNodeEvent(2ul, 3ul, expectedTreeRoot)  // Level 2 calculated from 2 nodes at level 1
+        ]);
+
+        accumulator.GetSize().Should().Be(4);
+        accumulator.CalculateRoot().Should().Be(expectedTreeRoot);
+    }
+
+    [Test]
+    public void GetExportState_3Nodes_ReturnsCorrectState()
+    {
+        (ArbosStorage storage, _) = TestArbosStorage.Create();
+        MerkleAccumulator accumulator = new(storage);
+
+        accumulator.Append(new("0x0000000000000000000000000000000000000000000000000000000000000001"));
+        accumulator.Append(new("0x0000000000000000000000000000000000000000000000000000000000000002"));
+        accumulator.Append(new("0x0000000000000000000000000000000000000000000000000000000000000003"));
+
+        // Captured from Nitro
+        MerkleAccumulatorExportState expected = new(3,
+            new ValueHash256("0x68d4880e8b3b97fc57678c4dce33b2e0d84a06024476eceba62bdc2e07a1e279"),
+            [
+                new ValueHash256("0xc2575a0e9e593c00f959f8c92f12db2869c3395a3b0502d05e2516446f71f85b"),
+                new ValueHash256("0x50387073e2d4f7060a3c02c3c5268d8a72700a28b5cbd7e23314ae0e1ebda895")
+            ]);
+
+        accumulator.GetExportState().Should().BeEquivalentTo(expected);
+    }
+
+    [Test]
+    public void GetExportState_4Nodes_ReturnsCorrectState()
+    {
+        (ArbosStorage storage, _) = TestArbosStorage.Create();
+        MerkleAccumulator accumulator = new(storage);
+
+        accumulator.Append(new("0x0000000000000000000000000000000000000000000000000000000000000001"));
+        accumulator.Append(new("0x0000000000000000000000000000000000000000000000000000000000000002"));
+        accumulator.Append(new("0x0000000000000000000000000000000000000000000000000000000000000003"));
+        accumulator.Append(new("0x0000000000000000000000000000000000000000000000000000000000000004"));
+
+        // Captured from Nitro
+        MerkleAccumulatorExportState expected = new(4,
+            new ValueHash256("0x1e8cc8511a4954df48a80e5f5b8da3419a99ba3e7697574234e10893022167fc"),
+            [
+                new ValueHash256("0x0000000000000000000000000000000000000000000000000000000000000000"), // Level 0 is calculated and reset
+                new ValueHash256("0x0000000000000000000000000000000000000000000000000000000000000000"), // Level 1 is calculated and reset
+                new ValueHash256("0x1e8cc8511a4954df48a80e5f5b8da3419a99ba3e7697574234e10893022167fc")
+            ]);
+
+        accumulator.GetExportState().Should().BeEquivalentTo(expected);
+    }
+}
--- src/Nethermind.Arbitrum/Arbos/Storage/MerkleAccumulator.cs
+++ src/Nethermind.Arbitrum/Arbos/Storage/MerkleAccumulator.cs
@@ -1,12 +1,133 @@
-using Nethermind.Logging;
+using System.Numerics;
+using Nethermind.Core.Crypto;
+using Nethermind.Core.Extensions;
 
 namespace Nethermind.Arbitrum.Arbos.Storage;
 
+public record MerkleTreeNodeEvent(ulong Level, ulong NumLeaves, ValueHash256 Hash);
+public record MerkleAccumulatorExportState(ulong Size, ValueHash256 Root, IReadOnlyList<ValueHash256> Partials);
+
 public class MerkleAccumulator(ArbosStorage storage)
 {
+    private const int FirstPartialIndex = 2; // An offset for the first partial to be compatible with Nitro's layout.
     private readonly ArbosStorageBackedULong _sizeStorage = new(storage, 0);
 
-    public static void Initialize(ArbosStorage storage, ILogger logger)
+    public ulong GetSize()
     {
+        return _sizeStorage.Get();
+    }
+
+    public ValueHash256 CalculateRoot()
+    {
+        ulong size = _sizeStorage.Get();
+        if (size == 0)
+        {
+            return default;
+        }
+
+        ValueHash256 soFar = default;
+        ulong capacityInHash = 0;
+        ulong capacity = 1;
+        ulong partialsCount = CountPartials(size);
+
+        ReadOnlySpan<byte> emptyBytes = stackalloc byte[ValueHash256.MemorySize];
+        Span<byte> buffer = stackalloc byte[ValueHash256.MemorySize * 2];
+
+        for (ulong level = 0; level < partialsCount; level++)
+        {
+            ValueHash256 partial = GetPartial(level);
+            if (partial != default)
+            {
+                if (soFar == default)
+                {
+                    soFar = partial;
+                    capacityInHash = capacity;
+                }
+                else
+                {
+                    while (capacityInHash < capacity)
+                    {
+                        soFar.Bytes.CopyTo(buffer);
+                        emptyBytes.CopyTo(buffer[ValueHash256.MemorySize..]);
+
+                        soFar = storage.CalculateHash(buffer);
+                        capacityInHash *= 2;
+                    }
+
+                    partial.Bytes.CopyTo(buffer);
+                    soFar.Bytes.CopyTo(buffer[ValueHash256.MemorySize..]);
+
+                    soFar = storage.CalculateHash(buffer);
+                    capacityInHash = 2 * capacity;
+                }
+            }
+
+            capacity *= 2;
+        }
+
+        return soFar;
+    }
+
+    public IReadOnlyCollection<MerkleTreeNodeEvent> Append(ValueHash256 item)
+    {
+        ulong size = _sizeStorage.Increment();
+        ulong partialsCount = CountPartials(size - 1);
+        List<MerkleTreeNodeEvent> events = new();
+
+        ulong level = 0;
+        ValueHash256 soFar = ValueKeccak.Compute(item.Bytes);
+        while (true)
+        {
+            if (level == partialsCount) // Reached a new level
+            {
+                SetPartial(level, soFar);
+                return events;
+            }
+
+            ValueHash256 thisLevel = GetPartial(level);
+            if (thisLevel == default) // Found empty slot
+            {
+                SetPartial(level, soFar);
+                return events;
+            }
+
+            // Combine and carry to next level
+            soFar = ValueKeccak.Compute(Bytes.Concat(thisLevel.Bytes, soFar.Bytes));
+            SetPartial(level, default); // Clear this level
+
+            level += 1;
+            events.Add(new MerkleTreeNodeEvent(level, size - 1, soFar));
+        }
+    }
+
+    public MerkleAccumulatorExportState GetExportState()
+    {
+        ValueHash256 root = CalculateRoot();
+        ulong size = _sizeStorage.Get();
+        ulong partialsCount = CountPartials(size);
+        ValueHash256[] partials = new ValueHash256[partialsCount];
+
+        for (ulong i = 0; i < partialsCount; i++)
+        {
+            partials[i] = GetPartial(i);
+        }
+
+        return new(size, root, partials);
+    }
+
+    private void SetPartial(ulong level, ValueHash256 hash)
+    {
+        storage.Set(level + FirstPartialIndex, hash);
+    }
+
+    private ValueHash256 GetPartial(ulong level)
+    {
+        return storage.Get(level + FirstPartialIndex);
+    }
+
+    private static ulong CountPartials(ulong size)
+    {
+        int log2Ceil = 64 - BitOperations.LeadingZeroCount(size);
+        return (ulong)log2Ceil;
     }
 }
