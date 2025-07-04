e9d4b63cc94e59cb30581ce5a8e191510ffa5fdd
AnkushinDaniil <ankushin.daniil42@gmail.com>
2025-07-04 12:00:38 +0400

    revert

--- src/Nethermind.Arbitrum/Precompiles/ArbInfo.cs
+++ src/Nethermind.Arbitrum/Precompiles/ArbInfo.cs
@@ -1,7 +1,6 @@
 using Nethermind.Arbitrum.Arbos;
 using Nethermind.Core;
 using Nethermind.Evm;
-using Nethermind.Int256;
 
 namespace Nethermind.Arbitrum.Precompiles;
 
@@ -9,7 +8,7 @@ public static class ArbInfo
 {
     public static Address Address => ArbosAddresses.ArbInfoAddress;
 
-    public static UInt256 GetBalance(ArbitrumPrecompileExecutionContext context, Address account)
+    public static Int256.UInt256 GetBalance(ArbitrumPrecompileExecutionContext context, Address account)
     {
         context.Burn(GasCostOf.BalanceEip1884);
         return context.WorldState.GetBalance(account);
@@ -21,12 +20,5 @@ public static class ArbInfo
         byte[] code = context.WorldState.GetCode(account)!;
         context.Burn(GasCostOf.DataCopy * Math.Utils.Div32Ceiling((ulong)code.Length));
         return code;
-    }
-
-    public static UInt256 GetCurrentTxL1GasFees(ArbitrumPrecompileExecutionContext context)
-    {
-        // TODO: In the full implementation, this would access the posterFee from
-        // the transaction execution context passed through the VM.
-        return UInt256.Zero; // Placeholder - would contain actual posterFee in full implementation
     }
 }
