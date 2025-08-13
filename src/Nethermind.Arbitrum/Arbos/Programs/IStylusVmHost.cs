d99926801d8a23c045cd2778305aaa0c25033477
Tanishq Jasoria <jasoriatanishq@gmail.com>
2025-08-13 16:01:33 +0530

    Add interface to wire stylus evm

---
+++ src/Nethermind.Arbitrum/Arbos/Programs/IStylusVmHost.cs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using Nethermind.Core;
+using Nethermind.Evm;
+using Nethermind.Int256;
+
+namespace Nethermind.Arbitrum.Arbos.Programs;
+
+internal interface IStylusVmHost
+{
+    (byte[] ret, ulong cost, Exception? err) DoCall(
+        ExecutionType kind, Address to, ReadOnlySpan<byte> input,
+        ulong gasLeftReportedByRust, ulong gasRequestedByRust, in UInt256 value);
+
+    (Address created, byte[] returnData, ulong cost, Exception? err) DoCreate(
+        ReadOnlySpan<byte> initCode, in UInt256 endowment, UInt256? salt, ulong gas);
+}
