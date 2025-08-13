47d0950caed6ada7759aea4eb5c2089926349f5d
stavrosvl7 <stavrosvl7@gmail.com>
2025-08-13 19:23:44 +0300

    Changes
    Signed-off-by: stavrosvl7 <stavrosvl7@gmail.com>

--- src/Nethermind.Arbitrum/ArbitrumNethermindApi.cs
+++ src/Nethermind.Arbitrum/ArbitrumNethermindApi.cs
@@ -11,7 +11,7 @@ using static Nethermind.Api.NethermindApi;
 
 public class ArbitrumNethermindApi(Dependencies dependencies) : NethermindApi(dependencies)
 {
-    private IBlockhashProvider BlockHashProvider => Context.Resolve<IBlockhashProvider>();
+    public IBlockhashProvider BlockHashProvider => Context.Resolve<IBlockhashProvider>();
 
     public override IBlockchainBridge CreateBlockchainBridge()
     {
