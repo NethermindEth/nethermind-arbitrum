bc940f64b1b2ee4d4b9612ef0fb1acfe0e61b6c9
Damian Orzechowski <damian.orzechowski@outlook.com>
2025-08-13 16:55:14 +0200

    Adjust timestamp to parents when lower - prepare header

--- src/Nethermind.Arbitrum.Test/BlockProcessing/BlockProducerTests.cs
+++ src/Nethermind.Arbitrum.Test/BlockProcessing/BlockProducerTests.cs
@@ -21,6 +21,8 @@ namespace Nethermind.Arbitrum.Test.BlockProcessing
     [TestFixture]
     internal class BlockProducerTests
     {
+        public const int DefaultTimeout = 1000;
+
         [Test]
         public void BuildBlock_Always_StartsFromArbitrumInternalTransaction()
         {
@@ -49,7 +51,7 @@ namespace Nethermind.Arbitrum.Test.BlockProcessing
 
             var blockTracer = new BlockReceiptsTracer();
             var buildBlockTask = chain.BlockProducer.BuildBlock(chain.BlockTree.Head?.Header, blockTracer, payloadAttributes);
-            buildBlockTask.Wait(1000);
+            buildBlockTask.Wait(DefaultTimeout);
 
             //assert
             buildBlockTask.IsCompletedSuccessfully.Should().BeTrue();
@@ -117,7 +119,7 @@ namespace Nethermind.Arbitrum.Test.BlockProcessing
             var buildBlockTask =
                 chain.BlockProducer.BuildBlock(chain.BlockTree.BestSuggestedHeader, blockTracer, payloadAttributes);
 
-            buildBlockTask.Wait(TimeSpan.FromSeconds(1));
+            buildBlockTask.Wait(DefaultTimeout);
 
             //assert
             buildBlockTask.IsCompletedSuccessfully.Should().BeTrue();
@@ -127,6 +129,67 @@ namespace Nethermind.Arbitrum.Test.BlockProcessing
             buildBlock.Transactions.Length.Should().Be(2);
             var receipt = blockTracer.TxReceipts[1];
             receipt.Sender.Should().BeEquivalentTo(TestItem.AddressA);
+        }
+
+        [Test]
+        public void BuildBlock_WhenTimestampSmallerThanParents_SetsAsParents()
+        {
+            UInt256 l1BaseFee = 39;
+            var preConfigurer = (ContainerBuilder cb) =>
+            {
+                cb.AddScoped(new ArbitrumTestBlockchainBase.Configuration()
+                {
+                    SuggestGenesisOnStart = true,
+                    L1BaseFee = l1BaseFee,
+                    FillWithTestDataOnStart = false
+                });
+            };
+
+            ArbitrumRpcTestBlockchain chain = ArbitrumRpcTestBlockchain.CreateDefault(preConfigurer);
+
+            var header1 = new L1IncomingMessageHeader(ArbitrumL1MessageKind.L2Message, TestItem.AddressA, 1,
+                1500, null, l1BaseFee);
+
+            var payloadAttributes1 = new ArbitrumPayloadAttributes()
+            {
+                MessageWithMetadata = new MessageWithMetadata(new L1IncomingMessage(header1, null, null), 10),
+                Number = 1
+            };
+
+            var blockTracer = new BlockReceiptsTracer();
+            var buildBlockTask = chain.BlockProducer.BuildBlock(chain.BlockTree.Head?.Header, blockTracer, payloadAttributes1);
+            buildBlockTask.Wait(DefaultTimeout);
+
+            var block1 = buildBlockTask.Result;
+
+            chain.BlockTree.SuggestBlock(block1!);
+
+            ManualResetEventSlim blockProcessedEvent = new(false);
+            chain.BlockProcessor.BlockProcessed += (_, _) =>
+            {
+                chain.BlockTree.UpdateMainChain([block1!], true);
+                blockProcessedEvent.Set();
+            };
+
+            //2nd block
+            var header2 = new L1IncomingMessageHeader(ArbitrumL1MessageKind.L2Message, TestItem.AddressA, 2,
+                1200, null, l1BaseFee);
+            var payloadAttributes2 = new ArbitrumPayloadAttributes()
+            {
+                MessageWithMetadata = new MessageWithMetadata(new L1IncomingMessage(header2, null, null), 10),
+                Number = 2
+            };
+
+            blockProcessedEvent.Wait(DefaultTimeout);
+
+            buildBlockTask = chain.BlockProducer.BuildBlock(chain.BlockTree.Head?.Header, blockTracer, payloadAttributes2);
+            buildBlockTask.Wait(DefaultTimeout);
+
+            buildBlockTask.IsCompletedSuccessfully.Should().BeTrue();
+
+            var buildBlock = buildBlockTask.Result;
+            buildBlock.Should().NotBeNull();
+            buildBlock.Header.Timestamp.Should().Be(1500);
         }
     }
 }
