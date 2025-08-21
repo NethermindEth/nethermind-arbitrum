b24499166775b7f512c9415cc2167e84d3c2f429
Damian Orzechowski <114909782+damian-orzechowski@users.noreply.github.com>
2025-08-21 13:19:46 +0200

    Increment number of tries for retryable (#156)

--- src/Nethermind.Arbitrum/Execution/ArbitrumTransactionProcessor.cs
+++ src/Nethermind.Arbitrum/Execution/ArbitrumTransactionProcessor.cs
@@ -596,6 +596,7 @@ namespace Nethermind.Arbitrum.Execution
                 MaxRefund = availableRefund,
                 SubmissionFeeRefund = submissionFee
             };
+            retryable.IncrementNumTries();
 
             outerRetryTx.Hash = outerRetryTx.CalculateHash();
 
