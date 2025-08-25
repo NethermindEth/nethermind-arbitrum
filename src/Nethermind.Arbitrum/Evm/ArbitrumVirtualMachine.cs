1e18d5701fef9301686654e1d2dbb71ee38301c3
Pavlo Rytikov <pavlo@nethermind.io>
2025-08-25 17:40:10 +0300

    Fix null ref when there is not enough gas

--- src/Nethermind.Arbitrum/Evm/ArbitrumVirtualMachine.cs
+++ src/Nethermind.Arbitrum/Evm/ArbitrumVirtualMachine.cs
@@ -68,12 +68,23 @@ public sealed unsafe class ArbitrumVirtualMachine(
             CurrentRetryable = ArbitrumTxExecutionContext.CurrentRetryable,
             CurrentRefundTo = ArbitrumTxExecutionContext.CurrentRefundTo
         };
+
+        try
+        {
+            // Arbos opening could throw if there is not enough gas
+            context.ArbosState = ArbosState.OpenArbosState(WorldState, context, Logger);
+        }
+        catch (OutOfGasException)
+        {
+            ArbosState arbosState = ArbosState.OpenArbosState(WorldState, new ZeroGasBurner(), Logger);
+            state.GasAvailable = GetGasAvailableOnFailure(arbosState, context);
+            return new(output: default, precompileSuccess: false, fromVersion: 0, shouldRevert: true);
+        }
+
         //TODO: temporary fix but should change error management from Exceptions to returning errors instead i think
         bool unauthorizedCallerException = false;
         try
         {
-            context.ArbosState = ArbosState.OpenArbosState(WorldState, context, Logger);
-
             // Revert if calldata does not contain method ID to be called
             if (callData.Length < 4)
             {
@@ -113,7 +124,7 @@ public sealed unsafe class ArbitrumVirtualMachine(
             if (Logger.IsError) Logger.Error($"Precompiled contract ({precompile.GetType()}) execution exception", exception);
             unauthorizedCallerException = OwnerWrapper.UnauthorizedCallerException().Equals(exception);
             //TODO: Additional check needed for ErrProgramActivation --> add check when doing ArbWasm precompile
-            state.GasAvailable = context.ArbosState.CurrentArbosVersion >= ArbosVersion.Eleven ? (long)context.GasLeft : 0;
+            state.GasAvailable = GetGasAvailableOnFailure(context.ArbosState, context);
             return new(output: default, precompileSuccess: false, fromVersion: 0, shouldRevert: true);
         }
         finally
@@ -143,5 +154,10 @@ public sealed unsafe class ArbitrumVirtualMachine(
         }
 
         return new(executionOutput, precompileSuccess: success, fromVersion: 0, shouldRevert: !success);
+    }
+
+    private static long GetGasAvailableOnFailure(ArbosState arbosState, ArbitrumPrecompileExecutionContext context)
+    {
+        return arbosState.CurrentArbosVersion >= ArbosVersion.Eleven ? (long)context.GasLeft : 0;
     }
 }
