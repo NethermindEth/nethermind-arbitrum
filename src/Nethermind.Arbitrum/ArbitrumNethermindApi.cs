91e99598afb703348807ed0cd952fc34d01a1724
Damian Orzechowski <damian.orzechowski@outlook.com>
2025-09-05 14:50:44 +0200

    Cleanup

--- src/Nethermind.Arbitrum/ArbitrumNethermindApi.cs
+++ src/Nethermind.Arbitrum/ArbitrumNethermindApi.cs
@@ -1,53 +1,7 @@
-using Autofac;
 using Nethermind.Api;
-using Nethermind.Arbitrum.TransactionProcessing;
-using Nethermind.Blockchain;
-using Nethermind.Config;
-using Nethermind.Consensus;
-using Nethermind.Evm;
-using Nethermind.Facade;
-using Nethermind.Facade.Simulate;
 using static Nethermind.Api.NethermindApi;
 
+// Overrides moved to IOC
 public class ArbitrumNethermindApi(Dependencies dependencies) : NethermindApi(dependencies)
 {
-    public IBlockhashProvider BlockHashProvider => Context.Resolve<IBlockhashProvider>();
-
-    //public override IBlockchainBridge CreateBlockchainBridge()
-    //{
-    //    ReadOnlyBlockTree readOnlyTree = BlockTree!.AsReadOnly();
-
-    //    ArbitrumOverridableTxProcessingEnv txProcessingEnv = new(
-    //        WorldStateManager!.CreateOverridableWorldScope(),
-    //        readOnlyTree,
-    //        SpecProvider!,
-    //        LogManager);
-
-    //    SimulateReadOnlyBlocksProcessingEnvFactory simulateReadOnlyBlocksProcessingEnvFactory =
-    //        new SimulateReadOnlyBlocksProcessingEnvFactory(
-    //            WorldStateManager!,
-    //            readOnlyTree,
-    //            DbProvider!,
-    //            SpecProvider!,
-    //            SimulateTransactionProcessorFactory,
-    //            LogManager);
-
-    //    IMiningConfig miningConfig = ConfigProvider.GetConfig<IMiningConfig>();
-    //    IBlocksConfig blocksConfig = ConfigProvider.GetConfig<IBlocksConfig>();
-
-    //    return new BlockchainBridge(
-    //        txProcessingEnv,
-    //        simulateReadOnlyBlocksProcessingEnvFactory,
-    //        TxPool,
-    //        ReceiptFinder,
-    //        FilterStore,
-    //        FilterManager,
-    //        EthereumEcdsa,
-    //        Timestamper,
-    //        LogFinder,
-    //        SpecProvider!,
-    //        blocksConfig,
-    //        miningConfig.Enabled
-    //    );
-    //}
 }
