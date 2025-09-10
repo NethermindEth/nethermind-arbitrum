9ec47988a254fe40f4ed8ae6028fbd2beeab1438
Tanishq Jasoria <jasoriatanishq@gmail.com>
2025-09-10 17:43:29 +0530

    refac tracing

--- src/Nethermind.Arbitrum/Tracing/TracingInfo.cs
+++ src/Nethermind.Arbitrum/Tracing/TracingInfo.cs
@@ -95,7 +95,7 @@ public class TracingInfo
         TraceInstruction(new TraceMemory(), stackPop, Instruction.POP);
     }
 
-    public void CaptureEvmTraceForHostio(string name, ReadOnlySpan<byte> args, byte[] outs, ulong startInk,
+    public void CaptureEvmTraceForHostio(string name, ReadOnlySpan<byte> args, ReadOnlySpan<byte> outs, ulong startInk,
         ulong endInk)
     {
         _firstOpcodeInHostio = true;
@@ -117,13 +117,13 @@ public class TracingInfo
         switch (name)
         {
             case "read_args":
-                Capture(Instruction.CALLDATACOPY, outs, UInt256.Zero, UInt256.Zero, new UInt256((ulong)outs.Length));
+                Capture(Instruction.CALLDATACOPY, outs.ToArray(), UInt256.Zero, UInt256.Zero, new UInt256((ulong)outs.Length));
                 break;
 
             case "storage_load_bytes32":
                 if (args.Length < 32 || outs.Length < 32) return;
                 var key = args[..32];
-                var value = outs.AsSpan()[..32];
+                var value = outs[..32];
                 if (_storageCache.Load(new Hash256(key), new Hash256(value)))
                 {
                     Capture(Instruction.SLOAD, Array.Empty<byte>(), new UInt256(key));
@@ -148,7 +148,7 @@ public class TracingInfo
             case "transient_load_bytes32":
                 if (args.Length < 32 || outs.Length < 32) return;
                 Capture(Instruction.TLOAD, null, new UInt256(args[..32]));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "transient_store_bytes32":
@@ -160,7 +160,7 @@ public class TracingInfo
                 if (args.Length < 32 || outs.Length < 20) return;
                 var createValue = new UInt256(args[..32]);
                 var createCode = args[32..].ToArray();
-                var createAddress = new UInt256(outs.AsSpan()[..20]);
+                var createAddress = new UInt256(outs[..20]);
                 Capture(Instruction.CREATE, createCode, createValue, UInt256.Zero,
                     new UInt256((ulong)createCode.Length));
                 Capture(Instruction.POP, null, createAddress);
@@ -171,7 +171,7 @@ public class TracingInfo
                 var create2Value = new UInt256(args[..32]);
                 var create2Salt = new UInt256(args.Slice(32, 32));
                 var create2Code = args[64..].ToArray();
-                var create2Address = new UInt256(outs.AsSpan()[..20]);
+                var create2Address = new UInt256(outs[..20]);
                 Capture(Instruction.CREATE2, create2Code, create2Value, UInt256.Zero,
                     new UInt256((ulong)create2Code.Length), create2Salt);
                 Capture(Instruction.POP, null, create2Address);
@@ -186,7 +186,7 @@ public class TracingInfo
             case "return_data_size":
                 if (outs.Length < 4) return;
                 Capture(Instruction.RETURNDATASIZE, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..4]));
+                Capture(Instruction.POP, null, new UInt256(outs[..4]));
                 break;
 
             case "emit_log":
@@ -209,7 +209,7 @@ public class TracingInfo
             case "account_balance":
                 if (args.Length < 20 || outs.Length < 32) return;
                 Capture(Instruction.BALANCE, Array.Empty<byte>(), new UInt256(args[..20]));
-                Capture(Instruction.POP, Array.Empty<byte>(), new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, Array.Empty<byte>(), new UInt256(outs[..32]));
                 break;
 
             case "account_code":
@@ -221,131 +221,131 @@ public class TracingInfo
             case "account_code_size":
                 if (args.Length < 20 || outs.Length < 4) return;
                 Capture(Instruction.EXTCODESIZE, null, new UInt256(args[..20]));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..4]));
+                Capture(Instruction.POP, null, new UInt256(outs[..4]));
                 break;
 
             case "account_codehash":
                 if (args.Length < 20 || outs.Length < 32) return;
                 Capture(Instruction.EXTCODEHASH, null, new UInt256(args[..20]));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "block_basefee":
                 if (outs.Length < 32) return;
                 Capture(Instruction.BASEFEE, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "block_coinbase":
                 if (outs.Length < 20) return;
                 Capture(Instruction.COINBASE, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..20]));
+                Capture(Instruction.POP, null, new UInt256(outs[..20]));
                 break;
 
             case "block_gas_limit":
                 if (outs.Length < 8) return;
                 Capture(Instruction.GASLIMIT, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..8]));
+                Capture(Instruction.POP, null, new UInt256(outs[..8]));
                 break;
 
             case "block_number":
                 if (outs.Length < 8) return;
                 Capture(Instruction.NUMBER, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..8]));
+                Capture(Instruction.POP, null, new UInt256(outs[..8]));
                 break;
 
             case "block_timestamp":
                 if (outs.Length < 8) return;
                 Capture(Instruction.TIMESTAMP, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..8]));
+                Capture(Instruction.POP, null, new UInt256(outs[..8]));
                 break;
 
             case "chainid":
                 if (outs.Length < 8) return;
                 Capture(Instruction.CHAINID, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..8]));
+                Capture(Instruction.POP, null, new UInt256(outs[..8]));
                 break;
 
             case "contract_address":
                 if (outs.Length < 20) return;
                 Capture(Instruction.ADDRESS, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..20]));
+                Capture(Instruction.POP, null, new UInt256(outs[..20]));
                 break;
 
             case "evm_gas_left":
             case "evm_ink_left":
                 if (outs.Length < 8) return;
                 Capture(Instruction.GAS, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..8]));
+                Capture(Instruction.POP, null, new UInt256(outs[..8]));
                 break;
 
             case "math_div":
                 if (args.Length < 64 || outs.Length < 32) return;
                 Capture(Instruction.DIV, null, new UInt256(args[..32]), new UInt256(args.Slice(32, 32)));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "math_mod":
                 if (args.Length < 64 || outs.Length < 32) return;
                 Capture(Instruction.MOD, null, new UInt256(args[..32]), new UInt256(args.Slice(32, 32)));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "math_pow":
                 if (args.Length < 64 || outs.Length < 32) return;
                 Capture(Instruction.EXP, null, new UInt256(args[..32]), new UInt256(args.Slice(32, 32)));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "math_add_mod":
                 if (args.Length < 96 || outs.Length < 32) return;
                 Capture(Instruction.ADDMOD, null, new UInt256(args[..32]), new UInt256(args.Slice(32, 32)),
                     new UInt256(args.Slice(64, 32)));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "math_mul_mod":
                 if (args.Length < 96 || outs.Length < 32) return;
                 Capture(Instruction.MULMOD, null, new UInt256(args[..32]), new UInt256(args.Slice(32, 32)),
                     new UInt256(args.Slice(64, 32)));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "msg_sender":
                 if (outs.Length < 20) return;
                 Capture(Instruction.CALLER, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..20]));
+                Capture(Instruction.POP, null, new UInt256(outs[..20]));
                 break;
 
             case "msg_value":
                 if (outs.Length < 32) return;
                 Capture(Instruction.CALLVALUE, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "native_keccak256":
                 if (outs.Length < 32) return;
                 var keccakData = args.ToArray();
                 Capture(Instruction.KECCAK256, keccakData, UInt256.Zero, new UInt256((ulong)keccakData.Length));
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "tx_gas_price":
                 if (outs.Length < 32) return;
                 Capture(Instruction.GASPRICE, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..32]));
+                Capture(Instruction.POP, null, new UInt256(outs[..32]));
                 break;
 
             case "tx_ink_price":
                 if (outs.Length < 4) return;
                 Capture(Instruction.GASPRICE, null); // Assumed to be equivalent for tracing
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..4]));
+                Capture(Instruction.POP, null, new UInt256(outs[..4]));
                 break;
 
             case "tx_origin":
                 if (outs.Length < 20) return;
                 Capture(Instruction.ORIGIN, null);
-                Capture(Instruction.POP, null, new UInt256(outs.AsSpan()[..20]));
+                Capture(Instruction.POP, null, new UInt256(outs[..20]));
                 break;
 
             // Cases handled elsewhere or with no EVM equivalent
