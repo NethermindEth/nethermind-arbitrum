e0bc75824fbe7fb237fc3e3bcdec9ccfd42513d6
Stavros Vlachakis <89769224+svlachakis@users.noreply.github.com>
2025-09-15 17:52:26 +0300

    Sync nc master - Fix Genesis loading (#214)
    * Update Nethermind submodule to latest master
    
    * Sync with Master Fixes
    
    * Update Nethermind submodule to latest
    
    * MainProcessingContext changes
    
    * Remove unused methods

--- src/Nethermind.Arbitrum/Execution/ArbitrumBlockProcessor.cs
+++ src/Nethermind.Arbitrum/Execution/ArbitrumBlockProcessor.cs
@@ -95,19 +95,13 @@ namespace Nethermind.Arbitrum.Execution
             ITransactionProcessor txProcessor,
             IWorldState stateProvider,
             IBlockProductionTransactionPicker txPicker,
-            ILogManager logManager)
+            ILogManager logManager,
+            BlockValidationTransactionsExecutor.ITransactionProcessedEventHandler? transactionProcessedHandler = null)
             : IBlockProductionTransactionsExecutor
         {
             private readonly ITransactionProcessorAdapter _transactionProcessor = new BuildUpTransactionProcessorAdapter(txProcessor);
             private readonly ILogger _logger = logManager.GetClassLogger();
-
-            protected EventHandler<TxProcessedEventArgs>? _transactionProcessed;
-
-            event EventHandler<TxProcessedEventArgs>? IBlockTransactionsExecutor.TransactionProcessed
-            {
-                add => _transactionProcessed += value;
-                remove => _transactionProcessed -= value;
-            }
+            private BlockValidationTransactionsExecutor.ITransactionProcessedEventHandler? _transactionProcessedHandler = transactionProcessedHandler;
 
             event EventHandler<AddingTxEventArgs>? IBlockProductionTransactionsExecutor.AddingTransaction
             {
@@ -349,8 +343,7 @@ namespace Nethermind.Arbitrum.Execution
 
                     if (result)
                     {
-                        _transactionProcessed?.Invoke(this,
-                            new TxProcessedEventArgs(index, currentTx, receiptsTracer.TxReceipts[index]));
+                        _transactionProcessedHandler?.OnTransactionProcessed(new TxProcessedEventArgs(index, currentTx, receiptsTracer.TxReceipts[index]));
                     }
                     else
                     {
