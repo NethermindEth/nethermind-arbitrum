b48074c8a78e3c9fc95ec2e670eba7487ae9a76a
stavrosvl7 <stavrosvl7@gmail.com>
2025-09-25 16:58:47 +0300

    fixes

--- src/Nethermind.Arbitrum/Config/ArbitrumChainSpecBasedSpecProvider.cs
+++ src/Nethermind.Arbitrum/Config/ArbitrumChainSpecBasedSpecProvider.cs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 using Nethermind.Arbitrum.Arbos;
-using Nethermind.Core.Specs;
 using Nethermind.Logging;
 using Nethermind.Specs;
 using Nethermind.Specs.ChainSpecStyle;
