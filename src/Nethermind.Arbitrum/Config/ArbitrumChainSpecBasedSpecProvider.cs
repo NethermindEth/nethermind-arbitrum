bfe43bfab54423e326a056608fe0d7f64677d81f
stavrosvl7 <stavrosvl7@gmail.com>
2025-09-25 16:57:06 +0300

    Implement ArbitrumReleaseSpec and ArbitrumDynamicSpecProvider

--- src/Nethermind.Arbitrum/Config/ArbitrumChainSpecBasedSpecProvider.cs
+++ src/Nethermind.Arbitrum/Config/ArbitrumChainSpecBasedSpecProvider.cs
@@ -9,44 +9,55 @@ using Nethermind.Specs.ChainSpecStyle;
 
 namespace Nethermind.Arbitrum.Config;
 
-public sealed class ArbitrumChainSpecBasedSpecProvider(ISpecProvider baseSpecProvider, IArbosVersionProvider arbosVersionProvider) : SpecProviderDecorator(baseSpecProvider)
+public class ArbitrumChainSpecBasedSpecProvider : ChainSpecBasedSpecProvider
 {
-    // Even though we mutate the spec, this is fine as each scope has its own spec provider instance
-    public override IReleaseSpec GetSpecInternal(ForkActivation activation)
+    private readonly ulong _arbosVersion;
+
+    public ArbitrumChainSpecBasedSpecProvider(
+        ChainSpec chainSpec,
+        IArbosVersionProvider arbosVersionProvider,
+        ILogManager logManager)
+        : base(chainSpec, logManager)
     {
-        IReleaseSpec spec = base.GetSpecInternal(activation);
+        _arbosVersion = arbosVersionProvider.Get();
+    }
 
-        ReleaseSpec mutableSpec = (ReleaseSpec)spec;
-        ulong currentArbosVersion = arbosVersionProvider.Get();
+    protected override ReleaseSpec CreateEmptyReleaseSpec() => new ArbitrumReleaseSpec();
 
+    protected override ReleaseSpec CreateReleaseSpec(ChainSpec chainSpec, long releaseStartBlock, ulong? releaseStartTimestamp = null)
+    {
+        ArbitrumReleaseSpec releaseSpec = (ArbitrumReleaseSpec)base.CreateReleaseSpec(chainSpec, releaseStartBlock, releaseStartTimestamp);
+
+        ApplyArbitrumOverrides(releaseSpec);
+
+        return releaseSpec;
+    }
+
+    private void ApplyArbitrumOverrides(ArbitrumReleaseSpec spec)
+    {
         // Shanghai EIPs (ArbOS v11+)
-        bool shanghaiEnabled = currentArbosVersion >= ArbosVersion.Eleven;
-        mutableSpec.IsEip3651Enabled = shanghaiEnabled;
-        mutableSpec.IsEip3855Enabled = shanghaiEnabled;
-        mutableSpec.IsEip3860Enabled = shanghaiEnabled;
+        bool shanghaiEnabled = _arbosVersion >= ArbosVersion.Eleven;
+        spec.IsEip3651Enabled = shanghaiEnabled;
+        spec.IsEip3855Enabled = shanghaiEnabled;
+        spec.IsEip3860Enabled = shanghaiEnabled;
 
         // Cancun EIPs (ArbOS v20+)
-        bool cancunEnabled = currentArbosVersion >= ArbosVersion.Twenty;
-        mutableSpec.IsEip1153Enabled = cancunEnabled;
-        mutableSpec.IsEip4788Enabled = cancunEnabled;
-        mutableSpec.IsEip5656Enabled = cancunEnabled;
-        mutableSpec.IsEip6780Enabled = cancunEnabled;
+        bool cancunEnabled = _arbosVersion >= ArbosVersion.Twenty;
+        spec.IsEip1153Enabled = cancunEnabled;
+        spec.IsEip4788Enabled = cancunEnabled;
+        spec.IsEip5656Enabled = cancunEnabled;
+        spec.IsEip6780Enabled = cancunEnabled;
 
         // Prague EIPs (ArbOS v40+)
-        bool pragueEnabled = currentArbosVersion >= ArbosVersion.Forty;
-        mutableSpec.IsEip7702Enabled = pragueEnabled;
-        mutableSpec.IsEip7251Enabled = pragueEnabled;
-        mutableSpec.IsEip2537Enabled = pragueEnabled;
-        mutableSpec.IsEip7002Enabled = pragueEnabled;
-        mutableSpec.IsEip6110Enabled = pragueEnabled;
+        bool pragueEnabled = _arbosVersion >= ArbosVersion.Forty;
+        spec.IsEip7702Enabled = pragueEnabled;
+        spec.IsEip7251Enabled = pragueEnabled;
+        spec.IsEip2537Enabled = pragueEnabled;
+        spec.IsEip7002Enabled = pragueEnabled;
+        spec.IsEip6110Enabled = pragueEnabled;
 
-        // Disable contract code validation as Arbitrum stores Stylus bytecode in code storage
-        mutableSpec.IsEip3541Enabled = false;
-
-        // Precompiles
-        ((IReleaseSpec)mutableSpec).Precompiles.Add(ArbosAddresses.ArbSysAddress);
-        ((IReleaseSpec)mutableSpec).Precompiles.Add(ArbosAddresses.ArbGasInfoAddress);
-
-        return mutableSpec;
+        // Disable this EIP in Arbitrum
+        spec.IsEip3541Enabled = false;
     }
 }
+
