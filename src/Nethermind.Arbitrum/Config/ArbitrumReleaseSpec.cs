a411d92cfc8889aea7ae94adac061afb0136c09d
Stavros Vlachakis <89769224+svlachakis@users.noreply.github.com>
2025-09-26 12:10:57 +0300

    TxTypeHasPosterCost fix + NC master sync (#254)
    * TxTypeHasPosterCost fix and tests
    
    * update latest master master

---
+++ src/Nethermind.Arbitrum/Config/ArbitrumReleaseSpec.cs
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
+using Nethermind.Arbitrum.Arbos;
+using Nethermind.Core;
+using Nethermind.Specs;
+using System.Collections.Frozen;
+
+namespace Nethermind.Arbitrum.Config;
+
+public class ArbitrumReleaseSpec : ReleaseSpec
+{
+    public override FrozenSet<AddressAsKey> BuildPrecompilesCache()
+    {
+        var basePrecompiles = base.BuildPrecompilesCache();
+        HashSet<AddressAsKey> precompiles = new(basePrecompiles);
+        precompiles.Add(ArbosAddresses.ArbSysAddress);
+        precompiles.Add(ArbosAddresses.ArbGasInfoAddress);
+        return precompiles.ToFrozenSet();
+    }
+}
