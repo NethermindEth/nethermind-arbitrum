f636226b0483b9971de5160977edb79cc52bcf51
Stavros Vlachakis <89769224+svlachakis@users.noreply.github.com>
2025-10-17 17:50:33 +0300

    Sync NC master (#315)
    * sync nc master
    
    * bump up NUnit3TestAdapter version in order to match NC

--- src/Nethermind.Arbitrum/Precompiles/ArbitrumCodeInfoRepository.cs
+++ src/Nethermind.Arbitrum/Precompiles/ArbitrumCodeInfoRepository.cs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Demerzel Solutions Limited
+// SPDX-License-Identifier: LGPL-3.0-only
+
 using System.Diagnostics.CodeAnalysis;
 using Nethermind.Arbitrum.Precompiles.Parser;
 using Nethermind.Core;
@@ -5,13 +8,12 @@ using Nethermind.Core.Crypto;
 using Nethermind.Core.Specs;
 using Nethermind.Evm;
 using Nethermind.Evm.CodeAnalysis;
-using Nethermind.Evm.State;
 
 namespace Nethermind.Arbitrum.Precompiles;
 
-public class ArbitrumCodeInfoRepository(ICodeInfoRepository codeInfoRepository) : IOverridableCodeInfoRepository
+public class ArbitrumCodeInfoRepository(ICodeInfoRepository codeInfoRepository) : ICodeInfoRepository
 {
-    private readonly Dictionary<Address, ICodeInfo> _codeOverwrites = InitializePrecompiledContracts();
+    private readonly Dictionary<Address, ICodeInfo> _arbitrumPrecompiles = InitializePrecompiledContracts();
 
     private static Dictionary<Address, ICodeInfo> InitializePrecompiledContracts()
     {
@@ -28,13 +30,14 @@ public class ArbitrumCodeInfoRepository(ICodeInfoRepository codeInfoRepository) 
         };
     }
 
-    public bool IsPrecompile(Address address, IReleaseSpec spec) => spec.IsPrecompile(address);
     public ICodeInfo GetCachedCodeInfo(Address codeSource, bool followDelegation, IReleaseSpec vmSpec, out Address? delegationAddress)
     {
         delegationAddress = null;
-        return _codeOverwrites.TryGetValue(codeSource, out ICodeInfo result)
-            ? result
-            : codeInfoRepository.GetCachedCodeInfo(codeSource, followDelegation, vmSpec, out delegationAddress);
+
+        if (_arbitrumPrecompiles.TryGetValue(codeSource, out ICodeInfo arbResult))
+            return arbResult;
+
+        return codeInfoRepository.GetCachedCodeInfo(codeSource, followDelegation, vmSpec, out delegationAddress);
     }
 
     public ValueHash256 GetExecutableCodeHash(Address address, IReleaseSpec spec) =>
@@ -48,22 +51,4 @@ public class ArbitrumCodeInfoRepository(ICodeInfoRepository codeInfoRepository) 
 
     public bool TryGetDelegation(Address address, IReleaseSpec vmSpec, [NotNullWhen(true)] out Address? delegatedAddress) =>
         codeInfoRepository.TryGetDelegation(address, vmSpec, out delegatedAddress);
-
-    public void SetDelegation(IWorldState state, Address codeSource, Address authority, IReleaseSpec spec) =>
-        codeInfoRepository.SetDelegation(codeSource, authority, spec);
-
-    public bool TryGetDelegation(IReadOnlyStateProvider worldState, Address address, IReleaseSpec vmSpec, [NotNullWhen(true)] out Address? delegatedAddress) =>
-        codeInfoRepository.TryGetDelegation(address, vmSpec, out delegatedAddress);
-
-    public void SetCodeOverwrite(IReleaseSpec vmSpec, Address key, ICodeInfo value, Address? redirectAddress = null)
-    {
-        if (redirectAddress is not null)
-        {
-            _codeOverwrites[redirectAddress] = this.GetCachedCodeInfo(key, vmSpec);
-        }
-
-        _codeOverwrites[key] = value;
-    }
-
-    public void ResetOverrides() => _codeOverwrites.Clear();
 }
