8b9f168badbf63178766ff80a65fa395bb54ea92
stavrosvl7 <stavrosvl7@gmail.com>
2025-10-21 18:11:22 +0300

    remove comment

--- src/Nethermind.Arbitrum/Precompiles/PrecompileHelper.cs
+++ src/Nethermind.Arbitrum/Precompiles/PrecompileHelper.cs
@@ -31,7 +31,7 @@ public static class PrecompileHelper
             _ when precompile is ArbGasInfoParser _ => CheckMethodVisibility<ArbGasInfoParser>(context, logger, ref calldata, out shouldRevert, out methodToExecute),
             _ when precompile is ArbAggregatorParser _ => CheckMethodVisibility<ArbAggregatorParser>(context, logger, ref calldata, out shouldRevert, out methodToExecute),
             _ when precompile is ArbActsParser _ => CheckMethodVisibility<ArbActsParser>(context, logger, ref calldata, out shouldRevert, out methodToExecute),
-            _ when precompile is ArbTestParser _ => CheckMethodVisibility<ArbTestParser>(context, logger, ref calldata, out shouldRevert, out methodToExecute), // Add this line
+            _ when precompile is ArbTestParser _ => CheckMethodVisibility<ArbTestParser>(context, logger, ref calldata, out shouldRevert, out methodToExecute),
             _ => throw new ArgumentException($"CheckMethodVisibility is not registered for precompile: {precompile.GetType()}")
         };
 
