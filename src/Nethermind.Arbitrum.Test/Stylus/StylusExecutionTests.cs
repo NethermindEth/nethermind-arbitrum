39a7618651f772dea3105a3ecf2efe87bf35d1fe
Damian Orzechowski <damian.orzechowski@outlook.com>
2025-11-17 14:55:10 +0100

    Add test

--- src/Nethermind.Arbitrum.Test/Stylus/StylusExecutionTests.cs
+++ src/Nethermind.Arbitrum.Test/Stylus/StylusExecutionTests.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using Nethermind.Abi;
 using Nethermind.Arbitrum.Data;
 using Nethermind.Arbitrum.Test.Infrastructure;
+using Nethermind.Blockchain;
 using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Core.Extensions;
@@ -208,6 +209,42 @@ public class StylusExecutionTests
             chain.WorldStateManager.GlobalWorldState.TryGetAccount(counterContract, out AccountStruct callAccountStruct);
             callAccountStruct.StorageRoot.Should().Be(storageTree.RootHash);
         }
+    }
+
+    [TestCase(StylusCallAddress, SolidityCounterAddress, 24, 21894L)]
+    [TestCase(StylusCallAddress, StylusCounterAddress, 24, 21882L)]
+    public async Task CallContract_CallCounterIncrement_ProxiesCallCalculatesCorrectGasSpent(string callAddress, string counterAddress, byte contractBlock, long expectedGas)
+    {
+        ArbitrumRpcTestBlockchain chain = new ArbitrumTestBlockchainBuilder()
+            .WithRecording(new FullChainSimulationRecordingFile(RecordingPath), contractBlock)
+            .Build();
+
+        Address sender = FullChainSimulationAccounts.Owner.Address;
+        Address callContract = new(callAddress);
+        Address counterContract = new(counterAddress);
+
+        Transaction callTransaction;
+
+        using (chain.WorldStateManager.GlobalWorldState.BeginScope(chain.BlockTree.Head?.Header))
+        {
+            // CALL increment through the Call contract
+            callTransaction = Build.A.Transaction
+                .WithType(TxType.EIP1559)
+                .WithTo(callContract)
+                .WithData(AbiEncoder.Instance.Encode(AbiEncodingStyle.IncludeSignature, ExecuteCallSignature, counterContract, CounterIncrementCalldata))
+                .WithMaxFeePerGas(10.GWei())
+                .WithGasLimit(500000)
+                .WithValue(0)
+                .WithNonce(chain.WorldStateManager.GlobalWorldState.GetNonce(sender))
+                .SignedAndResolved(FullChainSimulationAccounts.Owner)
+                .TestObject;
+        }
+
+        ResultWrapper<MessageResult> callResult = await chain.Digest(new TestL2Transactions(L1BaseFee, sender, callTransaction));
+        callResult.Result.Should().Be(Result.Success);
+        TxReceipt txReceipt = chain.LatestReceipts()[1];
+        txReceipt.StatusCode.Should().Be(StatusCode.Success);
+        txReceipt.GasUsed.Should().Be(expectedGas);
     }
 
     // TODO: implement STATICCALL test when EthRpcModule support is added to Test Blockchain
