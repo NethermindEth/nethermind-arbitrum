2aecf61724f62852cd9acd3a4c130ff0eb4a430b
AnkushinDaniil <ankushin.daniil42@gmail.com>
2025-12-31 16:07:54 +0400

    fix test

--- src/Nethermind.Arbitrum.Test/Infrastructure/ArbitrumRpcTestBlockchain.cs
+++ src/Nethermind.Arbitrum.Test/Infrastructure/ArbitrumRpcTestBlockchain.cs
@@ -381,7 +381,7 @@ public class ArbitrumRpcTestBlockchain : ArbitrumTestBlockchainBase
 
         public T UseArbosStorage<T>(Func<ArbosStorage, T> storageReader, BlockHeader? header = null)
         {
-            using IDisposable _ = chain.WorldStateManager.GlobalWorldState.BeginScope(header ?? chain.BlockTree.Head!.Header);
+            using IDisposable _ = chain.MainWorldState.BeginScope(header ?? chain.BlockTree.Head!.Header);
             ArbosStorage arbosStorage = new(chain.MainWorldState, new SystemBurner(), ArbosAddresses.ArbosSystemAccount);
             return storageReader(arbosStorage);
         }
