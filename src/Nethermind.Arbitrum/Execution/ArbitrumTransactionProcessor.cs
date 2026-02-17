4d2b418a1a483345685b86ed87a7deb306b9844e
Damian Orzechowski <damian.orzechowski@outlook.com>
2026-02-17 12:18:22 +0100

    Fix whitespace

--- src/Nethermind.Arbitrum/Execution/ArbitrumTransactionProcessor.cs
+++ src/Nethermind.Arbitrum/Execution/ArbitrumTransactionProcessor.cs
@@ -88,7 +88,7 @@ namespace Nethermind.Arbitrum.Execution
         }
 
         public override TransactionResult Warmup(Transaction transaction, ITxTracer txTracer) =>
-            Execute(transaction, txTracer,  ExecutionOptions.Warmup | ExecutionOptions.SkipValidation);
+            Execute(transaction, txTracer, ExecutionOptions.Warmup | ExecutionOptions.SkipValidation);
 
         protected override TransactionResult Execute(Transaction tx, ITxTracer tracer, ExecutionOptions opts)
         {
