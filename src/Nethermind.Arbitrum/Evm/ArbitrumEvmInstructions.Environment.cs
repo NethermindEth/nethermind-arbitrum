c7c9607938ebeeca01c2832a68aebacc5043cc55
Hugo Demeyere <demeyere.hugo@gmail.com>
2026-03-09 20:08:17 +0800

    fix Build

--- src/Nethermind.Arbitrum/Evm/ArbitrumEvmInstructions.Environment.cs
+++ src/Nethermind.Arbitrum/Evm/ArbitrumEvmInstructions.Environment.cs
@@ -159,7 +159,7 @@ internal static class ArbitrumEvmInstructions
     {
         IReleaseSpec spec = vm.Spec;
         // Deduct the gas cost for external code access.
-        TGasPolicy.Consume(ref gas, spec.GetExtCodeCost());
+        TGasPolicy.Consume(ref gas, spec.GasCosts.ExtCodeCost);
 
         // Pop the account address from the stack.
         Address? address = stack.PopAddress();
