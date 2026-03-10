18de00d0eb2870126b69a0099f578f1f5c6026e2
Pavlo Rytikov <pavlo@nethermind.io>
2026-03-10 18:53:26 +0200

    Use queue instead

--- src/Nethermind.Arbitrum/Sequencer/Queues/DelayedMessageQueue.cs
+++ src/Nethermind.Arbitrum/Sequencer/Queues/DelayedMessageQueue.cs
@@ -1,33 +1,29 @@
 // SPDX-License-Identifier: BUSL-1.1
 // SPDX-FileCopyrightText: https://github.com/NethermindEth/nethermind-arbitrum/blob/main/LICENSE.md
 
-using System.Threading.Channels;
+using System.Collections.Concurrent;
 using Nethermind.Arbitrum.Data;
 
 namespace Nethermind.Arbitrum.Sequencer.Queues;
 
 public class DelayedMessageQueue
 {
-    private volatile Channel<DelayedMessage> _channel = CreateChannel();
+    private volatile ConcurrentQueue<DelayedMessage> _queue = new();
     private volatile DelayedMessage? _tail;
 
     public void Enqueue(L1IncomingMessage[] messages, ulong firstMsgIdx)
     {
-        Channel<DelayedMessage> channel = _channel;
+        ConcurrentQueue<DelayedMessage> queue = _queue;
         for (int i = 0; i < messages.Length; i++)
         {
             DelayedMessage msg = new(messages[i], firstMsgIdx + (ulong)i);
-            channel.Writer.TryWrite(msg);
+            queue.Enqueue(msg);
             _tail = msg;
         }
     }
 
     public bool TryDequeue(out DelayedMessage? message)
-    {
-        bool result = _channel.Reader.TryRead(out DelayedMessage? msg);
-        message = msg;
-        return result;
-    }
+        => _queue.TryDequeue(out message);
 
     public bool TryPeekTail(out DelayedMessage? message)
     {
@@ -38,11 +34,6 @@ public class DelayedMessageQueue
     public void Clear()
     {
         _tail = null;
-        Channel<DelayedMessage> old = _channel;
-        _channel = CreateChannel();
-        old.Writer.Complete();
+        _queue = new();
     }
-
-    private static Channel<DelayedMessage> CreateChannel() =>
-        Channel.CreateUnbounded<DelayedMessage>(new UnboundedChannelOptions { SingleReader = true });
 }
