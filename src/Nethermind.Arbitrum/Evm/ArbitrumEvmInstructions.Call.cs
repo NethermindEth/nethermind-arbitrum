b8f1626187ad73bc034537326aad5a061e208b13
Hugo Demeyere <demeyere.hugo@gmail.com>
2026-04-04 19:16:20 +0800

    fix: Add license info

--- src/Nethermind.Arbitrum/Evm/ArbitrumEvmInstructions.Call.cs
+++ src/Nethermind.Arbitrum/Evm/ArbitrumEvmInstructions.Call.cs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BUSL-1.1
+// SPDX-FileCopyrightText: https://github.com/NethermindEth/nethermind-arbitrum/blob/main/LICENSE.md
 
 using System.Runtime.CompilerServices;
 using Nethermind.Core;
